ed372bdd98671181d7871cab9d272f95d7beee05
Kevin Gutierrez <kgutierrez@hugeinc.com>
2024-01-10 19:01:05 -0500

    complete example

--- tests/wizeline.spec.ts
+++ tests/wizeline.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 import { InventoryPage } from '../pages/inventory-page';
+import { ShoppingCart } from '../pages/shopping-cart';
 
 const URL = 'https://www.saucedemo.com/';
 let loginPage: LoginPage;
 let inventoryPage: InventoryPage;
+let shoppingCart: ShoppingCart;
 
 test.beforeEach(async ({page}) => {
     await page.goto(URL);
@@ -12,18 +14,19 @@ test.beforeEach(async ({page}) => {
     await loginPage.enterUsername();
     await loginPage.enterPassword();
     await loginPage.clickToLogin();
+    inventoryPage = new InventoryPage(page);
+    shoppingCart = new ShoppingCart(page);
 });
 
 test.describe('Sauce web labs website', () => {
-
-    // test('is the user logged in?', async () => {
-    //     await loginPage.enterUsername();
-    //     await loginPage.enterPassword();
-    //     await loginPage.clickToLogin();
-    // });
-
     test('add item sauce labs onesie to the cart', async () => {
         await inventoryPage.click_to_add_to_cart();
-      });
-      
+        await inventoryPage.assert_is_remote_cta_visible();
+        await inventoryPage.click_to_shopping_cart();
+    });
+    test('confirm correct item was added', async () => {
+        await shoppingCart.assert_item_information();
+    });
+
+
 });
